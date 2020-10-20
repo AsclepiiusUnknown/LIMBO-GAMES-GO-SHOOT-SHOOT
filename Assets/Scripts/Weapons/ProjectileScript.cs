3258e8b93b4ba12cf17633decbb02d23e0642c16
tylerwhelan <60990369+tylerwhelan@users.noreply.github.com>
2020-10-20 20:57:05 +1100

    Added weapon script and some other related things.

---
+++ Assets/Scripts/Weapons/ProjectileScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    private DamageReceiverScript owner;
+    public DamageReceiverScript Owner
+    {
+        get
+        {
+            return owner;
+        }
+    }
+    float damage;
+    float criticalDamage;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Collider mainCollider;
+
+    public void SetUpProjectile(float inDmg, float inCrit, DamageReceiverScript inOwner)
+    {
+        damage = inDmg;
+        criticalDamage = inCrit;
+        owner = inOwner;
+    }
+
+    public void BeginMovement()
+    {
+        rb.velocity = transform.forward * speed;
+        mainCollider.enabled = true;
+    }
+}
