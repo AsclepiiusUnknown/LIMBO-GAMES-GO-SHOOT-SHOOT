55b075492a27721b7f4434e384eb206be616fe06
Asclepiius.Unknown <asclepiius.unknown@gmail.com>
2020-10-21 13:01:14 +1100

    lunch

---
+++ Assets/Scripts/Brodie/HitReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LIMBO.Weaponary
+{
+    //Parent script for all damagable object/beings allowing for taking damage
+    public class HitReceiver : MonoBehaviour
+    {
+        [System.Serializable]
+        public class DeathEvent : UnityEvent<TargetJoint2D> { }
+
+
+    }
+}
---
+++ Assets/Scripts/Brodie/Movement/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LIMBO.Movement
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : Player
+    {
+
+    }
+}
