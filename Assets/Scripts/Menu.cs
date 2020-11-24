e0a3e99fcb995874409d6c7c9b3f7a7cc6595756
Asclepiius.Unknown <asclepiius.unknown@gmail.com>
2020-11-24 15:21:52 +1100

    Main Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void SpecialBtn()
+    {
+        LoadScene("Test Scene");
+    }
+
+    public void QuitBtn()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    void LoadScene(string _scene)
+    {
+        SceneManager.LoadScene(_scene);
+    }
+}
